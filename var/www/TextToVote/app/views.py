f589e50bb4f66a487b70af63b0723d89bc07d46b
Andrew Algard <andrewalgard@gmail.com>
2020-11-30 16:51:58 -0500

    update

--- var/www/TextToVote/app/views.py
+++ var/www/TextToVote/app/views.py
@@ -3,6 +3,7 @@ from flask import  request
 from flask_mysqldb import MySQL
 from app.submission import  Submission, SubmissionEncoder
 from app.message import Message, MessageEncoder
+from app.phone_number import PhoneNumber, PhoneNumberEncoder
 from twilio.twiml.messaging_response import MessagingResponse, Message
 import json
 mysql = MySQL(app)
@@ -50,11 +51,9 @@ def delete_submission():
 
 @app.route("/message", methods=['POST'])
 def message():
-    number = request.form["From"]
-    body = request.form["Body"]
-
+    form = request.form
     resp = MessagingResponse()
-    resp.message(body + "... in bed ;)")
+    resp.message(parse_message(form))
     return str(resp)
 
 
@@ -70,7 +69,7 @@ def phone_numbers():
         form = request.form
         try:
             print(str(form))
-            number = form["phone_number"]
+            number = form["From"]
             cur.execute("INSERT INTO phone_numbers(phone_number) VALUES(%s)", [str(number)])
             mysql.connection.commit()
             cur.close()
@@ -87,3 +86,99 @@ def messages():
     resp = map(lambda res: Message(res), msgs)
     return json.dumps(list(resp), cls=MessageEncoder)
 
+
+def get_or_create_number(body):
+    cursor = mysql.connection.cursor()
+    number = body["From"]
+    cursor.execute("Select * from phone_numbers where phone_number = %s", [str(number)])
+    num = cursor.fetchone()
+    cursor.close()
+    if num is None:
+        return num
+        # cursor = mysql.connection.cursor()
+        # cursor.execute("INSERT INTO phone_numbers(phone_number) VALUES(%s)", [str(number)])
+        # resp = mysql.connection.commit()
+        # cursor.close()
+        # return resp
+    else:
+        return PhoneNumber(num)
+
+
+def has_number_voted(number):
+    cursor = mysql.connection.cursor()
+    cursor.execute("Select * from phone_numbers where phone_number = %s", [str(number)])
+    num = cursor.fetchone()
+    cursor.close()
+    # returns true if number exists
+    return num is not None
+
+
+def parse_message(form):
+    cur = mysql.connection.cursor()
+    print(form)
+    string = form["Body"]
+    cur.execute("SELECT * FROM submissions WHERE abrev = %s", [string])
+    resp = cur.fetchone()
+    cur.close()
+    if resp is None:
+        return handle_non_vote(form["Body"])
+    else:
+        if has_number_voted(form["From"]):
+            return "Sorry, you can only vote once ;)"
+        else:
+            log_number(form["From"])
+            increment_submission(resp)
+            sub = Submission(resp)
+            return "Success! You voted for: {}".format(sub.name)
+
+
+def increment_submission(sub):
+    submission = Submission(sub)
+    new_count = (int(submission.votes) + 1)
+    cur = mysql.connection.cursor()
+    cur.execute(
+        " UPDATE submissions "
+        " SET votes = %s "
+        " WHERE id = %s ",
+        (new_count, submission.id)
+                )
+    mysql.connection.commit()
+    cur.close()
+
+
+def handle_non_vote(string):
+    if string.upper() == "SUBS":
+        return build_subs_list_message(
+            get_all_submissions()
+        )
+    else:
+        return "Sorry, your text didn't match any submissions we have. \n" \
+               "Text SUBS to see a list of submissions you can vote for."
+
+
+def get_all_submissions():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM submissions")
+    resp = cur.fetchall()
+    return map(lambda res: Submission(res), resp)
+
+
+def build_subs_list_message(subs):
+    msg = "VOTE for your favorite film! \n \n"
+
+    for i, submission in enumerate(subs):
+        line = "{index}. Text {abrev} for {name} \n".format(
+            index=(i + 1),
+            abrev=submission.abrev,
+            name=submission.name
+        )
+        msg = msg + line
+    return msg
+
+
+def log_number(number):
+    cursor = mysql.connection.cursor()
+    cursor.execute("INSERT INTO phone_numbers(phone_number) VALUES(%s)", [str(number)])
+    mysql.connection.commit()
+    cursor.close()
+    return
